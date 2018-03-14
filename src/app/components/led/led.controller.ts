63923a090650aedfe75a37c11f2eb38b7ce2c7d1
BETTY BRICE <ES662@CMB.PLATINE.ORG>
2018-03-14 14:34:02 +0100

    Mise à jour du "select" du label "Led"

--- src/app/components/led/led.controller.ts
+++ src/app/components/led/led.controller.ts
@@ -1,5 +1,5 @@
-import {Led} from '../../models/Led';
-import {LedService} from '../../services/led/led.service';
+import { Led } from '../../models/Led';
+import { LedService } from '../../services/led/led.service';
 import * as angular from 'angular';
 
 export class LedController {
@@ -7,14 +7,18 @@ export class LedController {
   ledService: LedService;
   leds: Array<Led>;
   idRuban: number;
-  
-  constructor (ledService: LedService) {
+  expanded = false;
+  isCheck = false;
+  elements: NodeListOf<HTMLInputElement>;
+
+
+  constructor(ledService: LedService) {
     'ngInject';
     this.ledService = ledService;
   }
 
 
-  $onInit () {
+  $onInit() {
     console.log('init led component' + this.idRuban);
   }
 
@@ -22,6 +26,32 @@ export class LedController {
     if (changesObj.idRuban) {
       this.idRuban = Number(angular.copy(changesObj.idRuban.currentValue));
       this.leds = this.ledService.getLeds(this.idRuban);
+    }
+  }
+
+  showCheckboxes() {
+    var checkboxes = document.getElementById("checkboxes");
+    if (!this.expanded) {
+      checkboxes.style.display = "block";
+      this.expanded = true;
+    } else {
+      checkboxes.style.display = "none";
+      this.expanded = false;
+    }
+  }
+
+  Checkboxes() {
+    var element = <HTMLInputElement>document.getElementById("one");
+    if (element.checked) {
+      this.elements = document.getElementsByTagName("input")
+      for(var i=0; i<this.elements.length; i++){
+        this.elements[i].checked = true;
+      }
+    }else{
+      this.elements = document.getElementsByTagName("input")
+      for(var i=0; i<this.elements.length; i++){
+        this.elements[i].checked = false;
+      }
     }
   }
 
